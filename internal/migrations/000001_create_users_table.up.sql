f1b7e90682c5775176c82ec1f5cb2fb3c7ac0d8e
Knave-De-Coeur <alexanderm.1496@gmail.com>
2023-04-17 10:52:22 +0200

    Added 1st migration and updated docker-compose.yml with redis.

---
+++ internal/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,4 @@
+insert into users (id, created_at, updated_at, deleted_at, first_name, last_name, email, age, username, password, last_login_time_stamp)
+values  (1, NOW(), NOW(), null, 'Alexander', 'Mifsud', 'alexanderm1496@gmail.com', 27, 'alexm1496', 'pass123', null),
+        (2, NOW(), NOW(), null, 'John', 'Doe', 'johndoe14@yahoo.com', 14, 'johndoe14', 'pass14', null),
+        (3, NOW(), NOW(), null, 'Sarah', 'Smith', 'sarahsmith@gmail.com', 38, 'ssmith38', 'qwerty321', null);
